efb2625b1616a99b626987210ce98fe3d9552698
TechnoAndy <47176316+TechnoAndy@users.noreply.github.com>
2022-10-04 23:40:34 +0200

    Create Task1.js
    Add code for stringLength

---
+++ Task1.js
@@ -0,0 +1,6 @@
+const stringLength = (string) => {
+    if (stringLength.length > 1 && stringLength.length < 10) return string.length;
+    else {throw Error ('The string exceeds the expected length')};
+}
+
+
